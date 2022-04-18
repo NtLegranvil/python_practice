2671e953fd9cb44514bf9ef4f3abec1bb4c96163
NtLegranvil <98447241+NtLegranvil@users.noreply.github.com>
2022-04-17 21:06:28 -0700

    Create welcome_message.py

---
+++ welcome_message.py
@@ -0,0 +1,8 @@
+"""
+Create A Welcome Message for a user
+"""
+# this program allows for user input to print a welcome message
+first_name = input("May I have your first name, please? ") # allow user to input first name into first_name variable
+last_name = input("May I have your last name, please? ") # allow user to input last name into last_name variable
+print("Thank you.") #output initial message
+print("\nWelcome, It's a pleasure to meet you " + first_name + " " + last_name + ".") # print message including users first and last name
